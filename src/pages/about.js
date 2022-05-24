7052e27ef46b9f0a35e7230869a9f7af042fe1a2
JMG24a <jmg24a@gmail.com>
2022-05-24 12:39:40 -0400

    [FIX] image 1.0

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,13 +1,12 @@
 import Image from "next/image"
 import { H2 } from "../styles/components/contact"
-const cv = require('../../public/images/cv.png');
 
 export default function About() {
   return (
     <>
       <H2>Sobre mi: </H2>
       <div style={{margin: '-20px auto 0px', width: '80%'}}>
-        <Image src={cv} alt='cv'/>
+        <Image src={'/public/images/cv.png'} alt='cv' width={'100px'} height='100px'/>
       </div>
     </>
   )
